5d3b41a7f287a5a344a4ddf5eddce6540100ab34
SashaMixaylov <alexandr_mixalov@mail.ru>
2025-03-17 12:52:03 +0300

    Логгер
    Создать приложение, следящее за работой пользователя на компьютере.
    Приложение (можно реализовать как комплекс приложений).
    
    Настройка, Статистика, Модерирование, Отчетность.
    
    Что конкретно выполнять, определяется пользователем на этапе настройки.
    
    Статистика-приложение незаметно для текущего пользователя выполняет процесс слежения.
    Если активирована статистика, приложение записывает информацию о всех нажатиях
    клавиш в файл отчета (путь настраивается пользователем через визуальный интерфейс),
    о всех запускаемых приложениях (в файл отчета должна попадать информация о названии запущенного приложения и
    времени запуска).
    
    Модерирование-приложение может следить за нажатиями клавиш на клавиатуре, за списком запущенных приложений.
    Приложение собирает статистику и производит модерирование.
    Если активировано модерирование, приложение анализирует клавиши,
    нажатые пользователем. В том случае, если было набрано слово из предопределённого списка (список слов указывается при настройке приложения),
    создаётся специальный файл отчета.
    При модерировании, если запускается запрещенная программа (список программ указывается при настройке приложения),
    приложение записывает информацию о запуске в файл отчета и
    закрывает запрещенную программу.
    
    В режиме отчетности пользователь может просмотреть информацию
    о клавишах и процессах (выбор пользовательского интерфейса остается за
    вами).

---
+++ WindowsFormsApp1_7.03.2025/WindowsFormsApp1_7.03.2025/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApp1_7._03._2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 78);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(256, 369);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(401, 78);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(188, 360);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(726, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(74, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(473, 13);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(457, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(726, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "StartTest";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(666, 181);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(726, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Close);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1_7.03.2025/WindowsFormsApp1_7.03.2025/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace WindowsFormsApp1_7._03._2025
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(Int32 vKey);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern UInt32 GetWindowThreadProcessId(IntPtr hwnd, ref Int32 pid);
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public string buf = "";
+        public string path1 = "./testIsFile.txt";
+        public string path2 = "./testIsFile2.txt";
+        public string[] mas = null;
+        private Timer timer;
+       // private Timer timer2;
+        private const int MOUSE_LEFTDOWN = 0x02;
+        private const int MOUSE_LEFTUP = 0x04;
+
+        private bool[] keyStates = new bool[256];
+        private const int VK_LSHIFT = 0xA0;
+        private const int VK_RSHIFT = 0xA1;
+        private const int VK_LCONTROL = 0xA2;
+        private const int VK_RCONTROL = 0xA3;
+        private const int VK_LMENU = 0xA4;
+        private const int VK_RMENU = 0xA5;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int key = 7; key < 256; key++) 
+            {richTextBox1.Text += key + "-" + ((KeysRu) key).ToString() + "/n"; 
+            }
+           
+            //Получаем объект CultureInfo для текущего потока (обычно соответствует языку операционной системы)
+            CultureInfo culture = CultureInfo.CurrentCulture;
+            //получаем код языка (например, «en» для английского, «fr» для французского и т. д.)
+            string languageCode = culture.TwoLetterISOLanguageName;
+
+            //получаем полное название языка (английский, французский)
+            string languageName = culture.DisplayName;
+
+            timer = new Timer();  // Таймер для автокликов
+            timer.Interval = 100; // Интервал между кликами в миллисекундах
+            timer.Tick += TimerClick;
+
+            //timer2 = new Timer();
+           // timer2.Interval = 1000; 
+           //timer2.Tick += timer2Click;
+            //Залипание клавиш
+            this.KeyDown += FormKeyDown;
+            this.KeyUp += FormKeyUp;
+
+            richTextBox2.Text += "код языка: " + languageCode + "\n";
+            richTextBox2.Text += "название кода: " + languageName + "\n";
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            int key113 = 0;
+            int key70 = 0;
+            int key87 = 0;
+            int key82 = 0;
+            int key79 = 0;
+            
+
+
+            for (int key = 7; key < 256; key++)
+            {
+                int state = GetAsyncKeyState(key);
+
+                if (state != 0)
+                {
+                    richTextBox1.Text += "{" + key + "-" + ((KeysRu)key).ToString() + "}";
+                    buf += ((KeysRu)key).ToString();
+
+                    if (key == 113)
+                        key113 = 1;
+                    if (key == 70)
+                        key70 = 1;
+                    if (key == 87)
+                        key87 = 1;
+                    if (key == 82)
+                        key82 = 1;
+                    if (key == 79)
+                        key79 = 1;
+
+                }
+
+            }
+
+            if (key113 != 0 && key70 != 0)
+            {
+                Process.Start(@"C:\Program Files\Mozilla Firefox", "https://visualstudio.microsoft.com/ru/");
+            }
+            if (key113 != 0 && key87 != 0)
+            {
+                Process.Start(@"C:\Program Files\Mozilla Firefox", "https://learn.microsoft.com/ru-ru/dotnet/api/system.windows.forms.keys?view=windowsdesktop-8.0");
+            }
+            if (key113 != 0 && key82 != 0)
+            {
+                Process.Start(@"C:\Program Files\Mozilla Firefox", "https://yandex.ru/images/");
+            }
+            if (key113 != 0 && key79 != 0) 
+            {
+                Process.Start("C:\\Windows\\System32\notepad.exe");
+            }
+
+            if (buf.Length > 20)
+            {
+                WriteToText(buf);
+                buf = "";
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Получаем активное окно
+                IntPtr h = GetForegroundWindow();
+                if (h == IntPtr.Zero) return;
+
+                int pid = 0;
+                GetWindowThreadProcessId(h, ref pid);
+
+                // Получаем информацию о процессе
+                try
+                {
+                    Process p = Process.GetProcessById(pid);
+
+                    // Формируем строку с информацией
+                    string info = $"Id: {p.Id} | ProcessName: {p.ProcessName} | Title: {p.MainWindowTitle}";
+
+                    // Добавляем в richTextBox2 с временной меткой
+                    richTextBox2.Text += $"{DateTime.Now:HH:mm:ss} - {info}\n";
+
+                    // Записываем в лог-файл
+                    WriteToText2($"{DateTime.Now:HH:mm:ss} - {info}\n");
+
+                    // Проверяем и блокируем нежелательные процессы
+                    if (mas.Contains(p.ProcessName))
+                    {
+                        p.Kill();
+                        MessageBox.Show($"Процесс '{p.ProcessName}' был завершен", "Контроль процессов");
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    // Пропускаем системные процессы
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при обработке процесса: {ex.Message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Критическая ошибка: {ex.Message}");
+            }
+
+        }
+
+
+        private void WriteToText(string value) 
+        {
+            StreamWriter stream = new StreamWriter(path1, true);
+            stream.Write(value);
+            stream.Close();
+        }
+
+        private void WriteToText2(string value) 
+        {
+            StreamWriter stream = new StreamWriter(path2, true);
+            stream.Write(value);
+            stream.Close();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.FormBorderStyle= FormBorderStyle.None;
+            this.Width = 0;
+            this.Height = 0;
+            this.ShowInTaskbar = false;
+            mas = richTextBox3.Text.Split(';');
+            if (textBox1.Text != "")
+                path1 = textBox1.Text;
+            if (textBox2.Text != "")
+                path2 = textBox2.Text;
+
+            if(checkBox1.Checked)
+            timer1.Start();
+
+            if(checkBox2.Checked)
+            timer2.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mas = richTextBox3.Text.Split(';');
+            if(textBox1.Text !="")
+                path1 = textBox1.Text;
+            if(textBox2.Text !="")
+                path2 = textBox2.Text;
+
+            if (checkBox1.Checked)
+                timer1.Start();
+
+            if (checkBox2.Checked)
+                timer2.Start();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) 
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            if (filename.IndexOf("txt") > 0)
+            {
+
+                string fileText = System.IO.File.ReadAllText(filename);
+
+                richTextBox1.Text = fileText;
+                MessageBox.Show("файл открыт");
+            }
+
+        }
+
+        private void TimerClick(object sender, EventArgs e)     //Автоклик
+        {
+            // Симулируем нажатие и отпускание левой кнопки мыши
+            mouse_event(MOUSE_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSE_LEFTUP, 0, 0, 0, 0);
+        }
+
+        //Залипание клавиш
+        private void FormKeyDown(object sender, KeyEventArgs e)
+        {
+            
+            keyStates[(int)e.KeyCode] = true;            // Сохраняем состояние клавиши
+        }
+
+        private void FormKeyUp(object sender, KeyEventArgs e)
+        {
+            
+            keyStates[(int)e.KeyCode] = false;         // Сбрасываем состояние клавиши
+        }
+
+        protected void Dispose(bool disposing)      //Освобождение рсурсов
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+            {
+               
+                timer1.Dispose();
+                timer2.Dispose();
+            }
+        }
+        private void button3_Close(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+        }
+    }
+}
