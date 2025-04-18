af828976f2086d38e457bef8d31aa352784ebea6
SashaMixaylov <alexandr_mixalov@mail.ru>
2025-04-18 16:04:34 +0300

    Исправление формы

---
+++ Chat/WinFormsApp1_Client_Num3/WinFormsApp1_Client_Num3/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WinFormsApp1_Client_Num3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            pictureBox1 = new PictureBox();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            contactList = new ListView();
+            groupList = new ListView();
+            openFileDialog1 = new OpenFileDialog();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(pictureBox1);
+            flowLayoutPanel1.Location = new Point(12, 12);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(409, 401);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(150, 89);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(461, 18);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(370, 395);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(15, 428);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(338, 55);
+            richTextBox2.TabIndex = 2;
+            richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.Fuchsia;
+            button1.Location = new Point(373, 429);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 54);
+            button1.TabIndex = 3;
+            button1.Text = "Send";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(47, 504);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Get_User";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(206, 505);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "Img";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(372, 500);
+            button4.Name = "button4";
+            button4.Size = new Size(107, 23);
+            button4.TabIndex = 6;
+            button4.Text = "Send Files";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(522, 429);
+            button5.Name = "button5";
+            button5.Size = new Size(83, 23);
+            button5.TabIndex = 7;
+            button5.Text = "Пригласить";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(654, 428);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 8;
+            button6.Text = "Создать";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(781, 429);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 9;
+            button7.Text = "Добавить";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // contactList
+            // 
+            contactList.Location = new Point(512, 458);
+            contactList.Name = "contactList";
+            contactList.Size = new Size(121, 80);
+            contactList.TabIndex = 10;
+            contactList.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupList
+            // 
+            groupList.Location = new Point(709, 461);
+            groupList.Name = "groupList";
+            groupList.Size = new Size(121, 77);
+            groupList.TabIndex = 11;
+            groupList.UseCompatibleStateImageBehavior = false;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(911, 548);
+            Controls.Add(groupList);
+            Controls.Add(contactList);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private PictureBox pictureBox1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private ListView contactList;
+        private ListView groupList;
+        private OpenFileDialog openFileDialog1;
+    }
+}
